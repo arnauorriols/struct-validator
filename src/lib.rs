5803fa6ae249efd2c9382536f6a071015c760d2f
Arnau Orriols <dev@arnauorriols.com>
2020-11-04 17:36:20 +0100

    Add TryFrom<&serde_json::Error>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::fmt::Display;
 use std::iter::Extend;
 use std::iter::FromIterator;
 
-use derive_more::{From, IntoIterator, Display, Error};
+use derive_more::{Display, Error, From, IntoIterator};
 use serde::{Deserialize, Serialize};
 
 pub use paste;
@@ -12,130 +12,138 @@ pub use paste;
 #[derive(Clone, Display, Error, Debug, Default, IntoIterator, From, Serialize, Deserialize)]
 #[display(fmt = "{}", "self.to_json_string()")]
 pub struct StructValidator {
-	pub errors: HashMap<String, String>,
+    pub errors: HashMap<String, String>,
 }
 
 impl StructValidator {
-	pub fn new() -> Self {
-		Self {
-			errors: HashMap::new(),
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            errors: HashMap::new(),
+        }
+    }
 
-	fn from_json_string<T>(str: T) -> Result<Self, serde_json::Error>
-	where
-		T: Into<String>,
-	{
-		let error_str = str.into();
-		let line_info_index = error_str
-			.rfind(" at line")
-			.unwrap_or_else(|| error_str.len());
-		let error_str = &error_str[..line_info_index];
-		serde_json::from_str(error_str)
-	}
+    fn from_json_string<T>(str: T) -> Result<Self, serde_json::Error>
+    where
+        T: Into<String>,
+    {
+        let error_str = str.into();
+        let line_info_index = error_str
+            .rfind(" at line")
+            .unwrap_or_else(|| error_str.len());
+        let error_str = &error_str[..line_info_index];
+        serde_json::from_str(error_str)
+    }
 
-	pub fn to_json_string(&self) -> String {
-		serde_json::to_string(self)
-			.unwrap_or_else(|e| format!("Error serializing StructValidator: {}", e))
-	}
+    pub fn to_json_string(&self) -> String {
+        serde_json::to_string(self)
+            .unwrap_or_else(|e| format!("Error serializing StructValidator: {}", e))
+    }
 
-	pub fn insert<K, V>(&mut self, key: K, value: V)
-	where
-		K: Into<String>,
-		V: Into<String>,
-	{
-		let error_str = value.into();
-		let line_info_index = error_str
-			.rfind(" at line")
-			.unwrap_or_else(|| error_str.len());
-		let error_str = error_str[..line_info_index].to_string();
-		self.errors.insert(key.into(), error_str);
-	}
+    pub fn insert<K, V>(&mut self, key: K, value: V)
+    where
+        K: Into<String>,
+        V: Into<String>,
+    {
+        let error_str = value.into();
+        let line_info_index = error_str
+            .rfind(" at line")
+            .unwrap_or_else(|| error_str.len());
+        let error_str = error_str[..line_info_index].to_string();
+        self.errors.insert(key.into(), error_str);
+    }
 
-	pub fn with<K, V>(mut self, key: K, value: V) -> Self
-	where
-		K: Into<String>,
-		V: Into<String>,
-	{
-		self.insert(key, value);
-		self
-	}
+    pub fn with<K, V>(mut self, key: K, value: V) -> Self
+    where
+        K: Into<String>,
+        V: Into<String>,
+    {
+        self.insert(key, value);
+        self
+    }
 
-	pub fn with_result<K, V, T>(self, key: K, result: Result<T, V>) -> Self
-	where
-		K: Into<String>,
-		V: ToString,
-	{
-		if let Err(e) = result {
-			self.with(key, e.to_string())
-		} else {
-			self
-		}
-	}
+    pub fn with_result<K, V, T>(self, key: K, result: Result<T, V>) -> Self
+    where
+        K: Into<String>,
+        V: ToString,
+    {
+        if let Err(e) = result {
+            self.with(key, e.to_string())
+        } else {
+            self
+        }
+    }
 
-	pub fn contains<T>(&self, key: T) -> bool
-	where
-		T: Into<String>,
-	{
-		self.errors.contains_key(&key.into())
-	}
+    pub fn contains<T>(&self, key: T) -> bool
+    where
+        T: Into<String>,
+    {
+        self.errors.contains_key(&key.into())
+    }
 
-	pub fn is_empty(&self) -> bool {
-		self.errors.is_empty()
-	}
+    pub fn is_empty(&self) -> bool {
+        self.errors.is_empty()
+    }
+}
+
+impl TryFrom<&serde_json::Error> for StructValidator {
+    type Error = serde_json::Error;
+
+    fn try_from(error: &serde_json::Error) -> Result<Self, serde_json::Error> {
+        Self::from_json_string(error.to_string())
+    }
 }
 
 impl TryFrom<serde_json::Error> for StructValidator {
-	type Error = serde_json::Error;
+    type Error = serde_json::Error;
 
-	fn try_from(error: serde_json::Error) -> Result<Self, serde_json::Error> {
-		Self::from_json_string(error.to_string())
-	}
+    fn try_from(error: serde_json::Error) -> Result<Self, serde_json::Error> {
+        Self::from_json_string(error.to_string())
+    }
 }
 
 impl serde::de::Error for StructValidator {
-	fn custom<T>(msg: T) -> Self
-	where
-		T: Display,
-	{
-		let mut errors = Self::new();
-		errors.insert("unknown".to_string(), msg.to_string());
-		errors
-	}
+    fn custom<T>(msg: T) -> Self
+    where
+        T: Display,
+    {
+        let mut errors = Self::new();
+        errors.insert("unknown".to_string(), msg.to_string());
+        errors
+    }
 }
 
 impl<'a, T: 'a> FromIterator<&'a Result<T, StructValidator>> for StructValidator {
-	fn from_iter<I>(iter: I) -> Self
-	where
-		I: IntoIterator<Item = &'a Result<T, StructValidator>>,
-	{
-		let mut errors = Self::new();
-		for item in iter {
-			if let Err(e) = item {
-				errors.extend(e.clone());
-			}
-		}
-		errors
-	}
+    fn from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = &'a Result<T, StructValidator>>,
+    {
+        let mut errors = Self::new();
+        for item in iter {
+            if let Err(e) = item {
+                errors.extend(e.clone());
+            }
+        }
+        errors
+    }
 }
 
 impl Extend<(String, String)> for StructValidator {
-	fn extend<T>(&mut self, iter: T)
-	where
-		T: IntoIterator<Item = (String, String)>,
-	{
-		self.errors.extend(iter)
-	}
+    fn extend<T>(&mut self, iter: T)
+    where
+        T: IntoIterator<Item = (String, String)>,
+    {
+        self.errors.extend(iter)
+    }
 }
 
 impl<'a, T: 'a> Extend<&'a Result<T, StructValidator>> for StructValidator {
-	fn extend<I>(&mut self, iter: I)
-	where
-		I: IntoIterator<Item = &'a Result<T, StructValidator>>,
-	{
-		let errors: StructValidator = iter.into_iter().collect();
-		self.extend(errors);
-	}
+    fn extend<I>(&mut self, iter: I)
+    where
+        I: IntoIterator<Item = &'a Result<T, StructValidator>>,
+    {
+        let errors: StructValidator = iter.into_iter().collect();
+        self.extend(errors);
+    }
 }
 
 #[macro_export]
